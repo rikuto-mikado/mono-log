f45473258fc3105c4349c228574334469a76cd4b
Rikuto <mikadorikuto@gmail.com>
2026-04-25 23:12:06 +0900

    start new project

---
+++ backend/notes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title or "Untitled Note"
+
+    class Meta:
+        ordering = ["-updated_at"]
